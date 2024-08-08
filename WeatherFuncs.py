f41bf362ef9069f2fd0f8808162f2c2d187b4391
Timm-Tomm <61246550+Timm-Tomm@users.noreply.github.com>
2024-08-08 17:05:57 -0500

    Add GUI

--- WeatherFuncs.py
+++ WeatherFuncs.py
@@ -1,15 +1,23 @@
 import requests
 import json
+import main
 from datetime import datetime
 from bs4 import BeautifulSoup
+from string import Template
+
 #API call to tomorrow.io for current weather data
 #Requires the location as a parameter to search on
 def get_current_weather(location):
-    url = "https://api.tomorrow.io/v4/weather/realtime?location={}%20US&units=imperial&apikey=eMQs1BYAUAZIDiEdTUMRvMve815lXnqm".format(location)
-    headers = {"accept": "application/json"}
-    response = requests.get(url, headers=headers)
-    return response.text
+    URL = "https://api.tomorrow.io/v4/weather/realtime?location={}%20US&units=imperial&apikey=eMQs1BYAUAZIDiEdTUMRvMve815lXnqm".format(location)
+    data = requests.get(URL) 
+    return data.json()
 
+""" def get_current_weather(location):
+    URL = Template("https://data.tomorrow.io/v4/weather/realtime?location=$loc%20US&units=imperial&apikey=eMQs1BYAUAZIDiEdTUMRvMve815lXnqm")
+    response = requests.get(url = URL.substitute(loc=location))
+    data = response.json()
+    return data['data']['timelines'][0]['intervals'][0]
+ """
 #API call to tomorrow.io for forecast data
 #Requires the location as a parameter to search on
 def get_hourly_forecast(location):
@@ -23,5 +31,5 @@ def get_History():
     r = requests.get("https://www.weather.gov/eax/wxhistory")
     soup = BeautifulSoup(r.content, 'html.parser')
     s = soup.find('tbody', class_="boldtype")
-    content = s.find_all('tr')
-    return content
+    #content = s.find_all('tr')
+    #return content
--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 from tkinter import *
 import WeatherFuncs
+import gui.py
 
 def main():
     #Initialize Window
     root =Tk()
-    root.geometry("400x400") #size of the window by default
+    #root.geometry("1080x720") #size of the window by default
     root.resizable(0,0) #to make the window size fixed
-    #title of our window
-    root.title("Weather App - AskPython.com")
-
-
-
-
-    #tfield = Text(root, width=46, height=10)
-    #tfield.pack()
     
+    #title of our window
+
+
+    #cur = WeatherFuncs.get_current_weather(64015)
+    #hour = WeatherFuncs.get_hourly_forecast(64015)
+    #hist = WeatherFuncs.get_History()
+    #print(cur)
+
+    canvas = Canvas(root, width=root.winfo_screenwidth(), height=root.winfo_screenheight(), bg="#87CEEB")
+
+    #canvas.create_text(300, 50, text=("Temperature: " + str(cur["data"]["values"]["temperature"]) + "°F"), fill="black")
+    canvas.pack()
+    
+    close = Button(canvas, text="Close", command=root.destroy, bg="grey")
+    close.place(x=root.winfo_screenwidth()-45, y=5)
+    root.wm_attributes('-fullscreen', 'True')
     root.mainloop()
+
+if __name__ == "__main__":
+    main()
